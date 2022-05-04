737336c07331fd842147b08866b892b37b554909
madhultchn <madhuhyd2021@gmail.com>
2022-05-04 20:35:21 +0530

    addedfile branc1

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -2,6 +2,6 @@ public class Test1 {
 
     public static void main(String[] args) {
 
-        System.out.println("test1branch2");
+        System.out.println("test1branch3");
     }
 }
--- src/main/java/com/example/demo/Test1branch1.java
+++ src/main/java/com/example/demo/Test1branch1.java
@@ -4,6 +4,6 @@ public class Test1branch1 {
 
     public static void main(String[] args) {
 
-        System.out.println("test1branch1 2");
+        System.out.println("test1branch1 3");
     }
 }
